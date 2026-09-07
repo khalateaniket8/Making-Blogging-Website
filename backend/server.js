4ad2b0326169b0227da79ce6b01c8845c9f33ecd
khalateaniket8 <khalateaniket8@gmail.com>
2026-09-07 18:58:29 +0530

    Fix backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,5 @@ app.get('/', (req, res) => {
   })
 })
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+// Export app for Vercel
+module.exports = app
